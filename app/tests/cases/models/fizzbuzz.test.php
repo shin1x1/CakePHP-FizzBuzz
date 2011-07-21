2086555a6115b62cef773736dbcb60f4284dff9a
shin1x1 <shin1x1@gmail.com>
2011-07-21 23:10:05 +0900

    modified assertEqual to assertIdentical

--- app/tests/cases/models/fizzbuzz.test.php
+++ app/tests/cases/models/fizzbuzz.test.php
@@ -4,25 +4,25 @@ App::import('Model', 'Fizzbuzz');
 class FizzBuzzTestCase extends CakeTestCase {
   public function test_makeFizzBuzzText_1() {
     $ret = Fizzbuzz::makeFizzBuzzText(1);
-    $this->assertEqual(1, $ret);
+    $this->assertIdentical('1', $ret);
   }
 
   public function test_makeFizzBuzzText_2() {
     $ret = Fizzbuzz::makeFizzBuzzText(2);
-    $this->assertEqual(2, $ret);
+    $this->assertIdentical('2', $ret);
   }
 
   public function test_makeFizzBuzzText_3() {
     $ret = Fizzbuzz::makeFizzBuzzText(3);
-    $this->assertEqual('Fizz', $ret);
+    $this->assertIdentical('Fizz', $ret);
   }
 
   public function test_makeFizzBuzzText_5() {
     $ret = Fizzbuzz::makeFizzBuzzText(5);
-    $this->assertEqual('Buzz', $ret);
+    $this->assertIdentical('Buzz', $ret);
   }
 
   public function test_makeFizzBuzzText_15() {
-    $this->assertEqual('FizzBuzz', Fizzbuzz::makeFizzBuzzText(15));
+    $this->assertIdentical('FizzBuzz', Fizzbuzz::makeFizzBuzzText(15));
   }
 }
